f5573c610b38f77ece7d42bad36a54fa2c405ac1
Thomas <ostrowski.thomas@gmail.com>
2015-12-07 23:35:55 +0100

    add creation table requests

--- script.sql
+++ script.sql
@@ -33,5 +33,26 @@ create table hotel (
 	id integer primary key auto_increment,
 	id_city integer not null,
 	name varchar(30) not null,
-	
+	constraint fk_city foreign key (id_city) references city(id)
+	on delete cascade
+	on update cascade 
+);
+
+create table category (
+	id integer primary key auto_increment,
+	id_hotel integer not null,
+	capacity integer not null,
+	price float(6,2) not null,
+	constraint fk_hotel foreign key (id_hotel) references hotel(id)
+	on delete cascade 
+	on update cascade
+);
+
+create table room (
+	id_hotel integer not null,
+	id_category integer not null,
+	room_number integer not null,
+	constraint pk_room primary key (id_hotel, id_category), 
+	constraint fk_hotel_room foreign key (id_hotel) references hotel(id),
+	constraint fk_category_room foreign key (id_category) references category(id)
 );
